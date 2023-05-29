448099a443457e7373ad504184841fe582943b83
RyotaFukumoto <f.ryota.828@gmail.com>
2023-05-29 21:36:02 +0900

    Initial Commit

---
+++ HighCohesivenessSample/HighCohesivenessSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HighCohesivenessSampleApp.swift
+//  HighCohesivenessSample
+//
+//  Created by ryota on 2023/05/29.
+//
+
+import SwiftUI
+
+@main
+struct HighCohesivenessSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
